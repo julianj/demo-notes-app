120235ee94d0e87e8a88f965043abb12a5e47627
Julian Jaramillo <julianj@gmail.com>
2023-02-19 15:24:15 -0800

    adding billing api

---
+++ services/functions/billing.js
@@ -0,0 +1,24 @@
+import Stripe from "stripe";
+import handler from "../util/handler";
+import { calculateCost } from "../util/cost";
+
+export const main = handler(async (event) => {
+    // storage is the number of notes the user would like to store in his account. 
+    // source is the Stripe token for the card that we are going to charge.
+    
+    const { storage, source } = JSON.parse(event.body);
+    const amount = calculateCost(storage);
+    const description = "Scratch charge";
+
+    // Load our secret key from the  environment variables
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+    await stripe.charges.create({
+        source,
+        amount,
+        description,
+        currency: "usd",
+    });
+
+    return { status: true };
+});
