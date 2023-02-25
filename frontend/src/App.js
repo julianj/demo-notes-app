fb7d940ee5bae0ac58d9614abaef16016466fcae
Julian Jaramillo <julianj@gmail.com>
2023-02-24 18:10:20 -0800

    changing scratch for JJ-Notas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ function App() {
         <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
           <LinkContainer to="/">
             <Navbar.Brand className="font-weight-bold text-muted">
-              Scratch
+              JJ-Notas
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle />
